d5c71fa21066d9801c3d2a48136dfb7fb195a62c
Dominika Bienkowska <bienkowska.dominika@gmail.com>
2015-03-10 13:14:24 +0100

    Initial Commit

---
+++ Web Service Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web Service Example
+//
+//  Created by Dominika on 10.03.2015.
+//  Copyright (c) 2015 DominikaBienkowska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
